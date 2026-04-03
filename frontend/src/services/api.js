7f061828de10d66a72dc471fea589ffacb9b65a2
GUILLE8022 <guillermoflexo@hotmail.com>
2026-04-03 16:19:24 -0500

    Cambiando api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const API = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: `${API_URL}/api`
 });
 
 // agregar token automáticamente
